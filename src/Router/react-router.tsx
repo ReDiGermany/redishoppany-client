27fcf2751db4b9bad80e61c2f01d57cb192cc9cd
Max Kruggel <support@redigermany.de>
2021-05-29 19:36:13 +0200

    Added different routing options for web/app

---
+++ src/Router/react-router.tsx
@@ -0,0 +1,12 @@
+import {
+  NativeRouter,
+  Route as NativeRoute,
+  Link as NativeLink,
+  useHistory as NativeUseHistory,
+} from 'react-router-native'
+
+export const Router = NativeRouter
+export const Route = NativeRoute
+export const Link = NativeLink
+export const UseHistory = NativeUseHistory
+// const history = useHistory()
---
+++ src/Router/react-router.web.tsx
@@ -0,0 +1,11 @@
+import {
+  BrowserRouter,
+  Route as WebRoute,
+  Link as WebLink,
+  useHistory as WebUseHistory,
+} from 'react-router-dom'
+
+export const Router = BrowserRouter
+export const Route = WebRoute
+export const Link = WebLink
+export const UseHistory = WebUseHistory
