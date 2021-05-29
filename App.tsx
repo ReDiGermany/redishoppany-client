b167884244d73ced34e791eaa9512d81d7dc94d6
Max Kruggel <support@redigermany.de>
2021-05-29 19:38:00 +0200

    Added Login functionallities

--- App.tsx
+++ App.tsx
@@ -1,8 +1,32 @@
 import React, { Component } from 'react'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 import Index from './src/Index'
+import SplashScreen from './src/SplashScreen'
+import Login from './src/pages/Login/Login'
+import APIUser from './src/helper/API/APIUser'
 
 export default class App extends Component {
+  state = {
+    checkMeDone: false,
+    loggedin: false,
+  }
+
+  async componentDidMount() {
+    try {
+      const token = (await AsyncStorage.getItem('redishoppany-token')) ?? ''
+      const email = (await AsyncStorage.getItem('redishoppany-email')) ?? ''
+      const me = await APIUser.getMeByToken(token, email)
+      this.setState({ checkMeDone: true, loggedin: me !== undefined })
+    } catch (error) {
+      this.setState({ checkMeDone: true, loggedin: false })
+      // Error retrieving data
+    }
+  }
+
   render() {
+    if (!this.state.checkMeDone) return <SplashScreen />
+    if (!this.state.loggedin) return <Login />
+
     return <Index />
   }
 }
