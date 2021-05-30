e1726501c93366a9050ec0d1805d31d54f8828d2
Max Kruggel <support@redigermany.de>
2021-05-30 18:01:47 +0200

    Added API Methods POST, GET, PUT, DELETE

--- src/helper/API.ts
+++ src/helper/API.ts
@@ -2,6 +2,13 @@ import axios, { AxiosInstance } from 'axios'
 import AsyncStorage from '@react-native-async-storage/async-storage'
 
 export default class API {
+  private initAPI() {
+    this.axiosInstance = axios.create({
+      baseURL: 'http://127.0.0.1:3001/',
+      timeout: 1000,
+    })
+  }
+
   private static INSTANCE: API
 
   private axiosInstance!: AxiosInstance
@@ -19,23 +26,48 @@ export default class API {
     return this.INSTANCE
   }
 
-  public static async get(uri: string) {
+  private static async getAuth() {
     const username = (await AsyncStorage.getItem('redishoppany-email')) ?? ''
     const password = (await AsyncStorage.getItem('redishoppany-token')) ?? ''
 
-    console.log('GETTING', uri, { username, password })
-
-    const ret = await this.axiosInstance.get(uri, {
+    return {
       auth: { username, password },
-    })
-
-    return ret.data
+    }
   }
 
-  private initAPI() {
-    this.axiosInstance = axios.create({
-      baseURL: 'http://127.0.0.1:3001/',
-      timeout: 1000,
-    })
+  public static async get<T>(uri: string): Promise<T> {
+    const auth = await this.getAuth()
+    console.log('GETTING', uri, auth)
+    const ret = await this.axiosInstance.get(uri, auth)
+
+    return ret.data.data
+  }
+
+  public static async post<T>(uri: string, data: any): Promise<T> {
+    const auth = await this.getAuth()
+    console.log('POSTING', uri, data, auth)
+    const ret = await this.axiosInstance.post(uri, data, auth)
+
+    return ret.data.data
+  }
+
+  public static async delete<T>(uri: string): Promise<T> {
+    const auth = await this.getAuth()
+    console.log('DELETING', uri, auth)
+    const ret = await this.axiosInstance.delete(uri, auth)
+
+    return ret.data.data
+  }
+
+  public static async put<T>(uri: string, data: any): Promise<T> {
+    const auth = await this.getAuth()
+    console.log('PUTTING', uri, data, auth)
+    const ret = await this.axiosInstance.put(uri, data, auth)
+
+    return ret.data.data
+  }
+
+  public static async putNoArgs<T>(uri: string): Promise<T> {
+    return this.put(uri, undefined)
   }
 }
