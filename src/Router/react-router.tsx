01e866c56112950843d99282a87bb6fd5b2b2376
Max Kruggel <support@redigermany.de>
2021-08-05 17:29:53 +0200

    Added redirect method

--- src/Router/react-router.tsx
+++ src/Router/react-router.tsx
@@ -3,10 +3,12 @@ import {
   Route as NativeRoute,
   Link as NativeLink,
   useHistory as NativeUseHistory,
+  Redirect as NativeRedirect,
 } from 'react-router-native'
 
 export const Router = NativeRouter
 export const Route = NativeRoute
 export const Link = NativeLink
 export const UseHistory = NativeUseHistory
+export const Redirect = NativeRedirect
 // const history = useHistory()
--- src/Router/react-router.web.tsx
+++ src/Router/react-router.web.tsx
@@ -3,9 +3,11 @@ import {
   Route as WebRoute,
   Link as WebLink,
   useHistory as WebUseHistory,
+  Redirect as WebRedirect,
 } from 'react-router-dom'
 
 export const Router = BrowserRouter
 export const Route = WebRoute
 export const Link = WebLink
 export const UseHistory = WebUseHistory
+export const Redirect = WebRedirect
