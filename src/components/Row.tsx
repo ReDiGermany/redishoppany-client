4bf51f33b1703b16b24cd74e51550d064054462b
Max Kruggel <support@redigermany.de>
2021-08-05 16:53:09 +0200

    Added styleability

--- src/components/Row.tsx
+++ src/components/Row.tsx
@@ -1,9 +1,14 @@
 import React, { Component } from 'react'
 import { View } from 'react-native'
 import RowStyle from '../styles/RowStyle'
+import IRow from '../interfaces/IRow'
 
-export default class Row extends Component {
+export default class Row extends Component<IRow> {
   render() {
-    return <View style={RowStyle}>{this.props.children}</View>
+    return (
+      <View style={{ ...RowStyle, ...this.props.style }}>
+        {this.props.children}
+      </View>
+    )
   }
 }
