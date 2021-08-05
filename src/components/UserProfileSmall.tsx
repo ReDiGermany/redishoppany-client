87db244f0a72dc8dfe9ff82bb98258f7e07e4e2f
Max Kruggel <support@redigermany.de>
2021-08-05 16:53:35 +0200

    Renamed + Moved + Added Bell

---
+++ src/components/UserProfileSmall.tsx
@@ -0,0 +1,43 @@
+import React, { Component } from 'react'
+import { View, Text } from 'react-native'
+import Icon from 'react-native-vector-icons/FontAwesome5'
+import IPageProps from '../interfaces/IPageProps'
+import { Link } from '../Router/react-router'
+import ColumnStyle from '../styles/ColumnStyle'
+import { profileStyle } from '../styles/SidebarStyle'
+import Row from './Row'
+
+export default class UserProfileSmall extends Component<IPageProps> {
+  render() {
+    console.log(this.props.user)
+    const icon = {
+      style: profileStyle.bell,
+      name: 'bell',
+    }
+
+    return (
+      <Row style={{}}>
+        <View style={profileStyle.image}></View>
+        <View style={ColumnStyle}>
+          <Text style={profileStyle.name}>
+            {this.props.user?.profile.firstName}{' '}
+            {this.props.user?.profile.lastName}
+          </Text>
+          <Text style={profileStyle.email}>
+            {this.props.user?.profile.email}
+          </Text>
+        </View>
+        {(this.props.user?.notificationCount ?? 0) > 0 && (
+          <Link style={profileStyle.bellLink} to="/notifications">
+            <>
+              <Icon solid={true} {...icon} />
+              <Text style={profileStyle.bellNumber}>
+                {this.props.user?.notificationCount}
+              </Text>
+            </>
+          </Link>
+        )}
+      </Row>
+    )
+  }
+}
