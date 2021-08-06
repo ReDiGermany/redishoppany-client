3d9b6ed9b1b0e0ca2057e2a5924f6b74bbacfea1
Max Kruggel <support@redigermany.de>
2021-08-06 15:53:40 +0200

    Added AddBar Added Fallback Icons if qr/addbar is open Removed unused stuff

--- src/pages/Home/HomeFriendlist.tsx
+++ src/pages/Home/HomeFriendlist.tsx
@@ -1,27 +1,15 @@
 import React, { Component } from 'react'
-import {
-  // RefreshControl, SafeAreaView,
-  ScrollView,
-  TextInput,
-  View,
-} from 'react-native'
+import { ScrollView, View } from 'react-native'
 import ListHeader from '../../ListHeader'
 import Moveable from '../../components/Moveable/Moveable'
 import QRCode from '../../QRCode'
 import GlobalStyles from '../../styles/GlobalStyles'
 import IPageProps from '../../interfaces/IPageProps'
 import Navigation from '../../Navigation'
+import AddBar from '../../components/AddBar'
 
 export default class Friends extends Component<IPageProps> {
   state = {
-    notifications: [
-      { prefix: 'heute', name: 'Curlybleu', empty: false },
-      { prefix: 'DO', name: 'Curry', empty: false },
-      { prefix: 'FR', name: 'Curry2', empty: false },
-      { prefix: 'SA', name: '', empty: true },
-      { prefix: 'SO', name: 'Curry3', empty: false },
-      { prefix: 'MO', name: '', empty: true },
-    ],
     refreshing: false,
     isTop: false,
     qr: false,
@@ -29,40 +17,8 @@ export default class Friends extends Component<IPageProps> {
   }
 
   render() {
-    // const onRefresh = () => {
-    //   this.setState({ refreshing: true })
-    //   setTimeout(() => {
-    //     this.setState({ refreshing: false })
-    //   }, 1000)
-    // }
-
     return (
       <View>
-        {/* <SafeAreaView
-          style={{
-            height:
-              GlobalStyles().appHeight -
-              GlobalStyles().barHeight -
-              GlobalStyles().statusbarHeight,
-          }}
-        >
-          <ScrollView
-            onScrollBeginDrag={() => {
-              this.setState({ scrolling: true })
-              console.log('scroll start')
-            }}
-            onScrollEndDrag={() => {
-              this.setState({ scrolling: false })
-              console.log('scroll stop')
-            }}
-            refreshControl={
-              <RefreshControl
-                refreshing={this.state.refreshing}
-                onRefresh={onRefresh}
-              />
-            }
-          > */}
-        {/* <UserProfileSmall solid={this.state.isTop} user={this.props.user} /> */}
         <Navigation
           label={this.props.user?.profile.firstName ?? 'User'}
           subTitle={this.props.user?.profile.email ?? 'someone@email.tld'}
@@ -70,17 +26,17 @@ export default class Friends extends Component<IPageProps> {
           solid={this.state.isTop}
           buttons={[
             {
-              icon: 'qrcode',
+              icon: this.state.qr ? 'chevron-up' : 'qrcode',
               name: 'qr',
               onClick: () => {
-                this.setState({ qr: !this.state.qr })
+                this.setState({ qr: !this.state.qr, add: false })
               },
             },
             {
-              icon: 'plus',
+              icon: this.state.add ? 'chevron-up' : 'plus',
               name: 'add',
               onClick: () => {
-                this.setState({ add: !this.state.add })
+                this.setState({ add: !this.state.add, qr: false })
               },
             },
           ]}
@@ -96,24 +52,16 @@ export default class Friends extends Component<IPageProps> {
             height: GlobalStyles().contentHeight - GlobalStyles().barHeight,
           }}
         >
-          {this.state.add && (
-            <TextInput
-              placeholder="E-Mail"
-              placeholderTextColor="#ffffff30"
-              style={{
-                margin: 10,
-                backgroundColor: '#11111180',
-                paddingVertical: 5,
-                paddingHorizontal: 10,
-                borderRadius: 10,
-              }}
-            />
-          )}
+          <AddBar
+            onChange={email => {}}
+            placeholder="E-Mail"
+            type="email"
+            visible={this.state.add}
+          />
           {this.state.qr && (
             <QRCode
               onFail={() => {
-                // eslint-disable-next-line no-alert
-                alert('Das hat leider nicht funktioniert.')
+                console.log('Das hat leider nicht funktioniert.')
               }}
               onSuccess={email => console.log(email)}
             />
@@ -137,8 +85,6 @@ export default class Friends extends Component<IPageProps> {
           <Moveable name="Tschink N." onDelete={() => {}} />
         </ScrollView>
       </View>
-      // </SafeAreaView>
-      // </View>
     )
   }
 }
