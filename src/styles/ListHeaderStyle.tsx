ee54db9c32eb2894f9a2a0844a38e90fee89233a
Max Kruggel <support@redigermany.de>
2021-08-06 16:32:52 +0200

    Updated Style

--- src/styles/ListHeaderStyle.tsx
+++ src/styles/ListHeaderStyle.tsx
@@ -1,14 +1,19 @@
 import { StyleSheet } from 'react-native'
 import GlobalStyles from './GlobalStyles'
-
+// GlobalStyles().dark.light
 export default StyleSheet.create({
   row: {
-    backgroundColor: GlobalStyles().dark.light,
-    color: GlobalStyles().dark.bright,
-    height: GlobalStyles().barHeight,
-    lineHeight: GlobalStyles().barHeight,
+    left: 10,
+    width: GlobalStyles().appWidth - 20,
+    marginTop: 10,
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    color: '#ffffff80',
+    height: 25,
+    lineHeight: 25,
     textAlignVertical: 'center',
     paddingLeft: 10,
-    fontSize: 20,
+    fontSize: 15,
+    textAlign: 'center',
   },
 }).row
