c0efc5c7fa7df74f96557df4f9445b513ff9d1d4
Max Kruggel <support@redigermany.de>
2021-08-06 17:17:26 +0200

    Added functionality

--- src/pages/Home/HomeFriendlist.tsx
+++ src/pages/Home/HomeFriendlist.tsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { ScrollView, View } from 'react-native'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 import ListHeader from '../../ListHeader'
 import Moveable from '../../components/Moveable/Moveable'
 import QRCode from '../../QRCode'
@@ -7,13 +8,52 @@ import GlobalStyles from '../../styles/GlobalStyles'
 import IPageProps from '../../interfaces/IPageProps'
 import Navigation from '../../Navigation'
 import AddBar from '../../components/AddBar'
+import APIFriends from '../../helper/API/APIFriends'
+import IPageState from '../../interfaces/IPageState'
+import IFriend from '../../interfaces/IFriend'
+import Language from '../../language/Language'
 
-export default class Friends extends Component<IPageProps> {
+export default class Friends extends Component<IPageProps, IPageState> {
   state = {
     refreshing: false,
     isTop: false,
     qr: false,
     add: false,
+    list: {
+      friends: [],
+      incomming: [],
+      outgoing: [],
+    },
+  }
+
+  constructor(props: IPageProps) {
+    super(props)
+    ;(async () => {
+      const list = await AsyncStorage.getItem('friendlist')
+      if (list != null) this.setState({ list: JSON.parse(list) })
+    })()
+  }
+
+  async componentDidMount() {
+    const list = await APIFriends.list()
+    this.setState({ list })
+    await AsyncStorage.setItem('friendlist', JSON.stringify(list))
+  }
+
+  cancleInvite(id: number) {
+    console.log('cancleInvite', id)
+  }
+
+  acceptFriend(id: number) {
+    console.log('acceptFriend', id)
+  }
+
+  removeFriend(id: number) {
+    console.log('removeFriend', id)
+  }
+
+  addFriend(email: string) {
+    console.log('addFriend', email)
   }
 
   render() {
@@ -53,7 +93,7 @@ export default class Friends extends Component<IPageProps> {
           }}
         >
           <AddBar
-            onChange={email => {}}
+            onChange={this.addFriend}
             placeholder="E-Mail"
             type="email"
             visible={this.state.add}
@@ -63,26 +103,59 @@ export default class Friends extends Component<IPageProps> {
               onFail={() => {
                 console.log('Das hat leider nicht funktioniert.')
               }}
-              onSuccess={email => console.log(email)}
+              onSuccess={this.addFriend}
             />
           )}
-          <ListHeader text="Freunde" />
-          <Moveable name="Freund N." onDelete={() => {}} />
-          <Moveable name="Alie N." onDelete={() => {}} />
-          <Moveable name="Attacke R." onDelete={() => {}} />
-          <Moveable name="Attacke R." onDelete={() => {}} />
-          <Moveable name="Attacke R." onDelete={() => {}} />
-          <Moveable name="Attacke R." onDelete={() => {}} />
-          <Moveable name="Attacke R." onDelete={() => {}} />
-          <Moveable name="Attacke R." onDelete={() => {}} />
-          <ListHeader text="Eingehende Anfragen" />
-          <Moveable
-            name="Zalam I."
-            onDelete={() => {}}
-            buttons={[{ name: 'accept', color: '#0F0', icon: 'check' }]}
-          />
-          <ListHeader text="Gesendete Anfragen" />
-          <Moveable name="Tschink N." onDelete={() => {}} />
+          {this.state.list.friends.length > 0 && (
+            <>
+              <ListHeader color="#111" text={Language.get('friends')} />
+              {this.state.list.friends.map((friend: IFriend, index) => (
+                <Moveable
+                  name={`${friend.firstName} ${friend.lastName}`}
+                  onDelete={() => this.removeFriend(friend.id)}
+                  last={index === this.state.list.friends.length - 1}
+                />
+              ))}
+            </>
+          )}
+          {this.state.list.incomming.length > 0 && (
+            <>
+              <ListHeader
+                color="#111"
+                text={Language.get('friends.incomming')}
+              />
+              {this.state.list.incomming.map((friend: IFriend, index) => (
+                <Moveable
+                  name={`${friend.firstName} ${friend.lastName}`}
+                  onDelete={() => this.cancleInvite(friend.id)}
+                  last={index === this.state.list.incomming.length - 1}
+                  buttons={[
+                    {
+                      name: 'accept',
+                      color: '#0F0',
+                      icon: 'check',
+                      onPress: () => this.acceptFriend(friend.id),
+                    },
+                  ]}
+                />
+              ))}
+            </>
+          )}
+          {this.state.list.outgoing.length > 0 && (
+            <>
+              <ListHeader
+                color="#111"
+                text={Language.get('friends.outgoing')}
+              />
+              {this.state.list.outgoing.map((friend: IFriend, index) => (
+                <Moveable
+                  name={`${friend.firstName} ${friend.lastName}`}
+                  onDelete={() => this.cancleInvite(friend.id)}
+                  last={index === this.state.list.outgoing.length - 1}
+                />
+              ))}
+            </>
+          )}
         </ScrollView>
       </View>
     )
