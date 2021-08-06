cd5520340dc6bd9112ee4cd5dcdcd324e42bd958
Max Kruggel <support@redigermany.de>
2021-08-06 16:31:31 +0200

    Added Background Color Prop

--- src/ListHeader.tsx
+++ src/ListHeader.tsx
@@ -4,10 +4,15 @@ import ListHeaderStyle from './styles/ListHeaderStyle'
 
 interface IListHeaderProps {
   text: string
+  color?: string
 }
 
 export default class ListHeader extends Component<IListHeaderProps> {
   render() {
-    return <Text style={ListHeaderStyle}>{this.props.text}</Text>
+    return (
+      <Text style={{ ...ListHeaderStyle, backgroundColor: this.props.color }}>
+        {this.props.text}
+      </Text>
+    )
   }
 }
