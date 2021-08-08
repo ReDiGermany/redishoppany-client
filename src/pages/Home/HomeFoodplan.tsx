74f7b4b2099625cbfee11880ed22b5eaa82db572
Max Kruggel <support@redigermany.de>
2021-08-08 11:56:21 +0200

    Added swipe Reload Added recipe adder when no recipe is given (deleted)

--- src/pages/Home/HomeFoodplan.tsx
+++ src/pages/Home/HomeFoodplan.tsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import {
+  RefreshControl,
   // RefreshControl, SafeAreaView,
   ScrollView,
   View,
@@ -15,17 +16,21 @@ import IFoodplanItem from '../../interfaces/IFoodplanItem'
 import APIRecipe from '../../helper/API/APIRecipe'
 import IFoodplanPageState from '../../interfaces/IFoodplanPageState'
 import { Redirect } from '../../Router/react-router'
+import ListHeader from '../../ListHeader'
+import IFoodplanKw from '../../interfaces/IFoodplanKw'
+import IMoveableProps from '../../interfaces/IMoveableProps'
 
 export default class Foodplan extends Component<
   IPageProps,
   IFoodplanPageState
 > {
-  state = {
+  state: IFoodplanPageState = {
     plan: [],
     recipes: [],
     refreshing: false,
     isTop: true,
     redirect: '',
+    item: undefined,
   }
 
   constructor(props: IPageProps) {
@@ -40,22 +45,37 @@ export default class Foodplan extends Component<
   }
 
   async componentDidMount() {
-    const plan = await APIFoodplan.list()
-    const recipes = await APIRecipe.list()
-    this.setState({ plan, recipes })
-    await AsyncStorage.setItem('recipeList', JSON.stringify(recipes))
-    await AsyncStorage.setItem('foodplanList', JSON.stringify(plan))
+    await this.refresh()
   }
 
   remove(item: IFoodplanItem) {
     ;(async () => {
       await APIFoodplan.remove(item.id)
       const { plan } = this.state
-      plan.forEach((itm: IFoodplanItem, idx) => {
-        if (itm.id === item.id) plan.splice(idx, 1)
+      plan.forEach((grp: IFoodplanKw, gidx) => {
+        grp.items.forEach((itm: IFoodplanItem, idx) => {
+          if (itm.id === item.id) plan.splice(idx, 1)
+        })
+        if (grp.items.length === 0) plan.splice(gidx, 1)
       })
       this.setState({ plan })
+      await this.refresh()
     })()
+  }
+
+  parseDate(date: string) {
+    return ['SO', 'MO', 'DI', 'MI', 'DO', 'FR', 'SA'][new Date(date).getDay()]
+  }
+
+  async refresh() {
+    this.setState({ refreshing: true })
+    const plan = await APIFoodplan.list()
+    const recipes = await APIRecipe.list()
+    this.setState({ plan, recipes })
+    await AsyncStorage.setItem('recipeList', JSON.stringify(recipes))
+    await AsyncStorage.setItem('foodplanList', JSON.stringify(plan))
+    console.log(plan)
+    this.setState({ refreshing: false })
   }
 
   render() {
@@ -80,6 +100,8 @@ export default class Foodplan extends Component<
         badge: { color: '#900000', text: this.props.user?.notificationCount },
       })
 
+    const currentDate = new Date()
+
     return (
       <View>
         <Navigation
@@ -88,25 +110,71 @@ export default class Foodplan extends Component<
           simple={true}
           buttons={buttons}
         />
-        <ScrollView
-          onScroll={e =>
-            this.setState({
-              isTop: e.nativeEvent.contentOffset.y <= 0,
-            })
-          }
-          style={{
-            height: GlobalStyles().contentHeight - GlobalStyles().barHeight,
-          }}
+        <RefreshControl
+          refreshing={this.state.refreshing}
+          onRefresh={() => this.refresh()}
         >
-          {this.state.plan.map((item: IFoodplanItem) => (
-            <Moveable
-              key={item.id}
-              onDelete={() => this.remove(item)}
-              prefix={item.date}
-              name={item.recipe.name}
-            />
-          ))}
-        </ScrollView>
+          <ScrollView
+            onScroll={e =>
+              this.setState({
+                isTop: e.nativeEvent.contentOffset.y <= 0,
+              })
+            }
+            style={{
+              height: GlobalStyles().contentHeight - GlobalStyles().barHeight,
+            }}
+          >
+            {this.state.plan.map((kw: IFoodplanKw) => (
+              <View key={kw.name}>
+                <ListHeader
+                  color="#111111"
+                  text={`KW ${kw.name.split('-')[1]}`}
+                />
+                {kw.items.map((item: IFoodplanItem, idx) => {
+                  if (item.recipe === null) {
+                    return (
+                      <Moveable
+                        key={`add_${item.date}`}
+                        name={`ADD RECIPE for ${this.parseDate(item.date)}`}
+                        centerText={true}
+                      />
+                    )
+                  }
+
+                  const mbuttons: any[] = []
+
+                  if (idx === 1)
+                    mbuttons.push({
+                      color: '#80dfad',
+                      icon: 'shopping-basket',
+                      name: 'cart',
+                      onPress: () => {
+                        this.setState({ item })
+                        // APIFoodplan.addToCart(item.id)
+                      },
+                    })
+                  const disabled = new Date(item.date) < currentDate
+                  if (disabled) mbuttons.splice(0)
+
+                  const moveable: IMoveableProps = {
+                    onDelete: undefined,
+                    prefix: this.parseDate(item.date),
+                    name: item.recipe.name ?? '?',
+                    last: idx === kw.items.length - 1,
+                    buttons: mbuttons,
+                    disabled,
+                  }
+
+                  if (!disabled) {
+                    moveable.onDelete = () => this.remove(item)
+                  }
+
+                  return <Moveable key={item.id} {...moveable} />
+                })}
+              </View>
+            ))}
+          </ScrollView>
+        </RefreshControl>
       </View>
     )
   }
