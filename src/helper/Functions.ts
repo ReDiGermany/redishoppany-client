9fb260bf1a65f416901dab92cc3886d6cf93d75a
Max Kruggel <support@redigermany.de>
2021-08-10 12:22:40 +0200

    Added helper functions

---
+++ src/helper/Functions.ts
@@ -0,0 +1,30 @@
+const inArray = (c: any, items: any[]) => {
+  for (let i = 0; i < items.length; i++) {
+    if (c === items[i]) return true
+  }
+
+  return false
+}
+
+const inString = (c: any, items: string) => inArray(c, items.split(''))
+
+const calcText = (inp: string) => {
+  let size = 0
+
+  for (let i = 0; i < inp.length; i++) {
+    const c = inp.charAt(i)
+    if (inString(c, 'fijlrtI')) {
+      size += 4
+    } else if (inString(c, 'abcdeghkmnopqsuvwxyz')) {
+      size += 8
+    } else if (inString(c, 'ABCDEFGHJKLMNOPQRSTUVWXYZ')) {
+      size += 9.5
+    } else {
+      size += 8
+    }
+  }
+
+  return size
+}
+
+export { inArray, inString, calcText }
