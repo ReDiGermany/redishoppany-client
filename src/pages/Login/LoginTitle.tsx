58f6f633cc8e271c02cd7313602695b9924b3c86
Max Kruggel <support@redigermany.de>
2021-08-10 11:29:55 +0200

    Added Header Language Added Subline

--- src/pages/Login/LoginTitle.tsx
+++ src/pages/Login/LoginTitle.tsx
@@ -1,20 +1,31 @@
 import React, { Component } from 'react'
-import { Text } from 'react-native'
+import { StyleSheet, Text } from 'react-native'
+import Language from '../../language/Language'
+
+const Style = StyleSheet.create({
+  headline: {
+    textAlignVertical: 'bottom',
+    textAlign: 'center',
+    fontSize: 20,
+    color: '#fff',
+    height: 30,
+  },
+  subline: {
+    textAlignVertical: 'bottom',
+    textAlign: 'center',
+    fontSize: 13,
+    color: '#ffffff80',
+    height: 20,
+  },
+})
 
 export default class LoginTitle extends Component {
   render() {
     return (
-      <Text
-        style={{
-          textAlignVertical: 'bottom',
-          textAlign: 'center',
-          fontSize: 20,
-          color: '#fff',
-          height: 30,
-        }}
-      >
-        Willkommen bei ReDiShoppany
-      </Text>
+      <>
+        <Text style={Style.headline}>{Language.get('welcome.headline')}</Text>
+        <Text style={Style.subline}>{Language.get('welcome.subline')}</Text>
+      </>
     )
   }
 }
