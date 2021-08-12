566af8e8a629314fb60a265a2716e09662f4ece0
Max Kruggel <support@redigermany.de>
2021-08-12 17:28:03 +0200

    Optimizations

--- App.tsx
+++ App.tsx
@@ -1,13 +1,13 @@
 import React, { Component } from 'react'
 import { AppearanceProvider } from 'react-native-appearance'
 import { StatusBar } from 'expo-status-bar'
-import { View, StatusBar as sb } from 'react-native'
+import { View } from 'react-native'
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import Index from './src/Index'
 import APIUser from './src/helper/API/APIUser'
 import Language from './src/language/Language'
-import GlobalStyles from './src/styles/GlobalStyles'
 import { Router } from './src/Router/react-router'
+import MainWindowStyles from './src/styles/MainWindowStyles'
 
 export default class App extends Component {
   state = {
@@ -36,22 +36,10 @@ export default class App extends Component {
     return (
       <AppearanceProvider>
         <StatusBar style="auto" />
-        <View
-          style={{
-            height: GlobalStyles().appHeight,
-            backgroundColor: '#202020',
-          }}
-        >
-          <View
-            style={{
-              marginTop: sb.currentHeight ?? 0,
-              height: GlobalStyles().appHeight,
-            }}
-          >
-            <Router>
-              <Index {...this.state} />
-            </Router>
-          </View>
+        <View style={MainWindowStyles.container}>
+          <Router>
+            <Index {...this.state} />
+          </Router>
         </View>
       </AppearanceProvider>
     )
---
+++ src/styles/MainWindowStyles.tsx
@@ -0,0 +1,11 @@
+import { StyleSheet } from 'react-native'
+import GlobalStyles from './GlobalStyles'
+
+const MainWindowStyles = StyleSheet.create({
+  container: {
+    height: GlobalStyles().appHeight,
+    backgroundColor: '#202020',
+  },
+})
+
+export default MainWindowStyles
