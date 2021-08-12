90b2e9004681d979d5fdbb9f2b8426b467e8438c
Max Kruggel <support@redigermany.de>
2021-08-12 23:36:01 +0200

    Added fullWidth

--- src/ListHeader.tsx
+++ src/ListHeader.tsx
@@ -1,16 +1,31 @@
 import React, { Component } from 'react'
 import { Text } from 'react-native'
+import GlobalStyles from './styles/GlobalStyles'
 import ListHeaderStyle from './styles/ListHeaderStyle'
 
 interface IListHeaderProps {
   text: string
   color?: string
+  fullWidth?: boolean
 }
 
 export default class ListHeader extends Component<IListHeaderProps> {
   render() {
     return (
-      <Text style={{ ...ListHeaderStyle, backgroundColor: this.props.color }}>
+      <Text
+        style={{
+          ...ListHeaderStyle,
+          backgroundColor: this.props.color,
+          ...(this.props.fullWidth
+            ? {
+                left: 0,
+                width: GlobalStyles().appWidth,
+                borderTopLeftRadius: 0,
+                borderTopRightRadius: 0,
+              }
+            : {}),
+        }}
+      >
         {this.props.text}
       </Text>
     )
