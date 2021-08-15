2a3fbadeaebe9db6f44cbaa2f2e58deb9ef3018f
Max Kruggel <support@redigermany.de>
2021-08-15 14:33:50 +0200

    Removed

--- src/components/Sidebar/SidebarLine.tsx
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react'
-import { View } from 'react-native'
-import { lineStyle } from '../../styles/SidebarStyle'
-
-export default class SidebarLine extends Component {
-  render() {
-    return <View style={lineStyle}></View>
-  }
-}
