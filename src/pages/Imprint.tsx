4bb43da4466a9ef815102057c36b58ad834e1e3a
Max Kruggel <support@redigermany.de>
2021-08-15 16:38:36 +0200

    Code Cleanup

--- src/pages/Imprint.tsx
+++ src/pages/Imprint.tsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { View, Text, ScrollView } from 'react-native'
+import { View, Text, ScrollView, StyleSheet } from 'react-native'
 import IPageProps from '../interfaces/IPageProps'
 import Language from '../language/Language'
 import Navigation from '../Navigation'
@@ -7,17 +7,20 @@ import GlobalStyles from '../styles/GlobalStyles'
 
 export default class Imprint extends Component<IPageProps> {
   render() {
+    const style = StyleSheet.create({
+      scrollView: {
+        width: '100%',
+        height: GlobalStyles().contentHeight,
+        padding: 20,
+      },
+      text: { color: '#fff' },
+    })
+
     return (
       <View>
         <Navigation user={this.props.user} label={Language.get('imprint')} />
-        <ScrollView
-          style={{
-            width: '100%',
-            height: GlobalStyles().contentHeight,
-            padding: 20,
-          }}
-        >
-          <Text style={{ color: '#fff' }}>{Language.get('imprint.text')}</Text>
+        <ScrollView style={style.scrollView}>
+          <Text style={style.text}>{Language.get('imprint.text')}</Text>
         </ScrollView>
       </View>
     )
