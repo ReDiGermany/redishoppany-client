146a7ca6a3c6a887fb01f0efb32b60595bd37675
Max Kruggel <support@redigermany.de>
2021-08-15 16:38:49 +0200

    Removed

--- src/pages/MainList.tsx
+++
@@ -1,8 +0,0 @@
-import React, { Component } from 'react'
-import IPageProps from '../interfaces/IPageProps'
-
-export default class MainList extends Component<IPageProps> {
-  render() {
-    return <MainList user={this.props.user} />
-  }
-}
