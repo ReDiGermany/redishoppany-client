4dcd7ca0d047ee06e0b6b1ba19a3aabbd628573e
Max Kruggel <support@redigermany.de>
2021-08-15 14:32:57 +0200

    Code Cleanup

--- src/components/Moveable/MoveableTextPrefix.tsx
+++ src/components/Moveable/MoveableTextPrefix.tsx
@@ -1,25 +1,19 @@
 import React, { Component } from 'react'
 import { Text } from 'react-native'
 import { textStyle } from '../../styles/MoveableStyle'
-
-interface IMoveableTextPrefixProps {
-  text: string | number
-  disabled?: boolean
-}
+import IMoveableTextPrefixProps from '../../interfaces/IMoveableTextPrefixProps'
 
 export default class MoveableTextPrefix extends Component<IMoveableTextPrefixProps> {
   render() {
-    return (
-      <Text
-        style={{
-          ...textStyle.left.prefix,
-          ...(this.props.disabled ?? false
-            ? { height: 30, lineHeight: 30, opacity: 0.3 }
-            : {}),
-        }}
-      >
-        {this.props.text.toString()}
-      </Text>
-    )
+    const style = {
+      ...textStyle.left.prefix,
+      ...((this.props.disabled ?? false) && {
+        height: 30,
+        lineHeight: 30,
+        opacity: 0.3,
+      }),
+    }
+
+    return <Text style={style}>{this.props.text.toString()}</Text>
   }
 }
---
+++ src/interfaces/IMoveableTextPrefixProps.tsx
@@ -0,0 +1,4 @@
+export default interface IMoveableTextPrefixProps {
+  text: string | number
+  disabled?: boolean
+}
