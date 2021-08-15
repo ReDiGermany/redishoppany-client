90c88556c7d333a643b690b0600f09ba5ceb148e
Max Kruggel <support@redigermany.de>
2021-08-15 16:45:06 +0200

    Code Cleanup

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,18 +1,16 @@
 import React, { Component } from 'react'
-import { View } from 'react-native'
-import IAPIUserMe from '../interfaces/IAPIUserMe'
+import { View, Text } from 'react-native'
 import Navigation from '../Navigation'
-
-interface IAboutProps {
-  user?: IAPIUserMe
-}
+import IAboutProps from '../interfaces/IAboutProps'
 
 export default class About extends Component<IAboutProps> {
   render() {
     return (
       <View>
         <Navigation user={this.props.user} label="About" />
-        {/* https://pixabay.com/de/photos/hochzeitsfeier-bankett-geschirr-1967373/ */}
+        <Text style={{ color: '#fff', padding: 20 }}>
+          https://pixabay.com/de/photos/hochzeitsfeier-bankett-geschirr-1967373/
+        </Text>
       </View>
     )
   }
