26cedb294d7002e6c75f8675cea069c5ddc9db1a
Max Kruggel <support@redigermany.de>
2021-08-15 15:18:26 +0200

    Code Cleanup

--- src/ListHeader.tsx
+++ src/ListHeader.tsx
@@ -12,26 +12,18 @@ interface IListHeaderProps {
 
 export default class ListHeader extends Component<IListHeaderProps> {
   render() {
-    return (
-      <Text
-        style={{
-          ...ListHeaderStyle,
-          backgroundColor: this.props.color,
-          ...(this.props.fullWidth
-            ? {
-                left: 0,
-                width: GlobalStyles().appWidth,
-                borderTopLeftRadius: 0,
-                borderTopRightRadius: 0,
-              }
-            : {}),
-          ...(GetLuma(this.props.color ?? '#111') > 150
-            ? { color: '#00000080' }
-            : {}),
-        }}
-      >
-        {this.props.text}
-      </Text>
-    )
+    const style = {
+      ...ListHeaderStyle,
+      backgroundColor: this.props.color,
+      ...(this.props.fullWidth && {
+        left: 0,
+        width: GlobalStyles().appWidth,
+        borderTopLeftRadius: 0,
+        borderTopRightRadius: 0,
+      }),
+      ...(GetLuma(this.props.color ?? '#111') > 150 && { color: '#00000080' }),
+    }
+
+    return <Text style={style}>{this.props.text}</Text>
   }
 }
