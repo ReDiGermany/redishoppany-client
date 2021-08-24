90a46ffa0c8f7edbad66cfa4b9711da7ec47ea24
Max Kruggel <support@redigermany.de>
2021-08-24 16:27:59 +0200

    Updated Error handling

--- src/helper/API.ts
+++ src/helper/API.ts
@@ -1,10 +1,13 @@
 import axios, { AxiosInstance } from 'axios'
 import AsyncStorage from '@react-native-async-storage/async-storage'
 
+// Error Handling from https://gist.github.com/fgilio/230ccd514e9381fafa51608fcf137253
+
 export default class API {
   private initAPI() {
     this.axiosInstance = axios.create({
       baseURL: 'https://api.lisha-app.com',
+      // baseURL: 'http://192.168.0.30:3001',
       timeout: 1000,
     })
   }
@@ -35,70 +38,105 @@ export default class API {
     }
   }
 
-  public static async get<T>(uri: string): Promise<T> {
+  public static async get<T>(uri: string): Promise<T | null> {
     const auth = await API.getAuth()
-    // console.log('GETTING::init', uri, auth)
-    const ret = await API.axiosInstance.get(uri, auth)
-    // console.log('GETTING::ret', ret)
-    // if (ret.status < 200 || ret.status > 299)
-    // console.log('AXIOS::ERROR::GET', { uri, auth, ret: JSON.stringify(ret) })
+    const ret = await API.axiosInstance.get(uri, auth).catch(error => {
+      console.log('AXIOS::GET::ERROR', { uri, auth })
+      // Error 😨
+      if (error.response) {
+        // The request was made and the server responded with a status code that falls out of the range of 2xx
+        console.log(
+          'error.response',
+          error.response.status,
+          error.response.statusText
+        )
+      } else if (error.request) {
+        // The request was made but no response was received, `error.request` is an instance of XMLHttpRequest in the browser and an instance of http.ClientRequest in Node.js
+        console.log('error.request', error.request)
+      } else {
+        // Something happened in setting up the request and triggered an Error
+        console.log('Error', error.message)
+      }
+    })
+    if (!ret) return null
 
     if (typeof ret.data === 'object' && 'data' in ret.data) return ret.data.data
 
     return ret.data
   }
 
-  public static async post<T>(uri: string, data: any): Promise<T> {
+  public static async post<T>(uri: string, data: any): Promise<T | null> {
     const auth = await API.getAuth()
-    // console.log('POSTING', uri, data, auth)
-    const ret = await API.axiosInstance.post(uri, data, auth)
-    // if (ret.status < 200 || ret.status > 299)
-    //   console.log('AXIOS::ERROR::POST', {
-    //     uri,
-    //     auth,
-    //     ret: JSON.stringify(ret),
-    //     data: JSON.stringify(data),
-    //   })
+    const ret = await API.axiosInstance.post(uri, data, auth).catch(error => {
+      console.log('AXIOS::POST::ERROR', { uri, auth, data })
+      // Error 😨
+      if (error.response) {
+        // The request was made and the server responded with a status code that falls out of the range of 2xx
+        console.log('error.response', error.response.status, error.response)
+      } else if (error.request) {
+        // The request was made but no response was received, `error.request` is an instance of XMLHttpRequest in the browser and an instance of http.ClientRequest in Node.js
+        console.log('error.request', error.request)
+      } else {
+        // Something happened in setting up the request and triggered an Error
+        console.log('Error', error.message)
+      }
+    })
+    if (!ret) return null
 
     if (typeof ret.data === 'object' && 'data' in ret.data) return ret.data.data
 
     return ret.data
   }
 
-  public static async delete<T>(uri: string): Promise<T> {
+  public static async delete<T>(uri: string): Promise<T | null> {
     const auth = await API.getAuth()
-    // console.log('DELETING', uri, auth)
-    const ret = await API.axiosInstance.delete(uri, auth)
-    // if (ret.status < 200 || ret.status > 299)
-    //   console.log('AXIOS::ERROR::DELETE', {
-    //     uri,
-    //     auth,
-    //     ret: JSON.stringify(ret),
-    //   })
+    const ret = await API.axiosInstance.delete(uri, auth).catch(error => {
+      console.log('AXIOS::DELETE::ERROR', { uri, auth })
+      // Error 😨
+      if (error.response) {
+        // The request was made and the server responded with a status code that falls out of the range of 2xx
+        console.log('error.response', error.response.status, error.response)
+      } else if (error.request) {
+        // The request was made but no response was received, `error.request` is an instance of XMLHttpRequest in the browser and an instance of http.ClientRequest in Node.js
+        console.log('error.request', error.request)
+      } else {
+        // Something happened in setting up the request and triggered an Error
+        console.log('Error', error.message)
+      }
+    })
+    if (!ret) return null
 
     if (typeof ret.data === 'object' && 'data' in ret.data) return ret.data.data
 
     return ret.data
   }
 
-  public static async put<T>(uri: string, data: any): Promise<T> {
+  public static async put<T>(uri: string, data: any): Promise<T | null> {
     const auth = await API.getAuth()
-    // console.log('PUTTING', uri, data, auth)
-    const ret = await API.axiosInstance.put(uri, data, auth)
-    // if (ret.status < 200 || ret.status > 299)
-    //   console.log('AXIOS::ERROR::PUT', {
-    //     uri,
-    //     auth,
-    //     ret: JSON.stringify(ret),
-    //     data: JSON.stringify(data),
-    //   })
+    const ret = await API.axiosInstance.put(uri, data, auth).catch(error => {
+      console.log('AXIOS::PUT::ERROR', { uri, auth, data })
+      // Error 😨
+      if (error.response) {
+        // The request was made and the server responded with a status code that falls out of the range of 2xx
+        console.log('error.response', error.response.status, error.response)
+      } else if (error.request) {
+        // The request was made but no response was received, `error.request` is an instance of XMLHttpRequest in the browser and an instance of http.ClientRequest in Node.js
+        console.log('error.request', error.request)
+      } else {
+        // Something happened in setting up the request and triggered an Error
+        console.log('Error', error.message)
+      }
+    })
+    if (!ret) return null
 
     if (typeof ret.data === 'object' && 'data' in ret.data) return ret.data.data
 
     return ret.data
   }
 
-  public static async putNoArgs<T>(uri: string): Promise<T> {
-    return this.put(uri, undefined)
+  public static async putNoArgs<T>(uri: string): Promise<T | null> {
+    const t = await API.put<T>(uri, undefined)
+
+    return t
   }
 }
