bba265b613c6f6438385b536e9f1c68d8a90159b
Max Kruggel <support@redigermany.de>
2021-08-24 14:07:42 +0200

    Updated register page

---
+++ src/interfaces/IRegisterState.ts
@@ -0,0 +1,16 @@
+import IAlertProps from './IAlertProps'
+
+export default interface IRegisterState {
+  firstName: string
+  lastName: string
+  password: string
+  passwordRepeat: string
+  email: string
+  keyboardHeight: number
+  checking: boolean
+  disabled: boolean
+  emailValid?: boolean
+  passwordValid?: boolean
+  passwordRepeatValid?: boolean
+  alert: IAlertProps
+}
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,40 +1,185 @@
 import React, { Component } from 'react'
+import { Text } from 'react-native'
+import { ScrollView } from 'react-native-gesture-handler'
+import Alert from '../components/Alert'
+import BackgroundImage from '../components/BackgroundImage'
+import APIUser from '../helper/API/APIUser'
+import {
+  ErrorAlert,
+  PreErrorAlert,
+  PreWarningAlert,
+  SuccessAlert,
+  WarningAlert,
+} from '../helper/DefinedAlerts'
 import Navigation from '../Navigation'
+import GlobalStyles, { KeyboardDetection } from '../styles/GlobalStyles'
+import IRegisterProps from '../interfaces/IRegisterProps'
 import LoginButton from './Login/LoginButton'
+import LoginInput from './Login/LoginInput'
 import LoginInputEmail from './Login/LoginInputEmail'
 import LoginInputPassword from './Login/LoginInputPassword'
 import RegisterTitle from './Login/RegisterTitle'
+import IRegisterState from '../interfaces/IRegisterState'
 
-export default class Register extends Component {
+export default class Register extends Component<
+  IRegisterProps,
+  IRegisterState
+> {
+  state: IRegisterState = {
+    firstName: '',
+    lastName: '',
+    password: '',
+    passwordRepeat: '',
+    keyboardHeight: 0,
+    email: '',
+    checking: false,
+    disabled: true,
+    emailValid: true,
+    alert: {
+      type: 'error',
+      text: '',
+    },
+    passwordValid: undefined,
+    passwordRepeatValid: undefined,
+  }
+
+  WAlert(pre: string): void {
+    this.setState({
+      alert: PreWarningAlert(`register.${pre}.`, 'text', 'info'),
+    })
+  }
+
+  async submit() {
+    this.setState({ checking: true })
+
+    const { firstName, lastName, email, password, passwordRepeat } = this.state
+
+    const pwValid = !this.state.passwordValid || !this.state.passwordRepeatValid
+    const pwMatch = password === '' || password !== passwordRepeat
+
+    if (firstName === '') return this.WAlert('no.firstname')
+    if (lastName === '') return this.WAlert('no.lastname')
+    if (email === '') return this.WAlert('no.email')
+    if (this.state.emailValid !== true) return this.WAlert('invalid.email')
+    if (pwValid) return this.WAlert('invalid.passwords')
+    if (pwMatch) return this.WAlert('unmatch.passwords')
+
+    try {
+      const account = await APIUser.register(
+        firstName,
+        lastName,
+        email,
+        password,
+        passwordRepeat
+      )
+
+      if (account) {
+        const alert = SuccessAlert('register.welcome')
+        alert.text.replaceAll('%firstname%', firstName)
+        this.setState({ alert })
+      } else {
+        this.setState({ alert: WarningAlert('register.unsuccess') })
+      }
+    } catch (e) {
+      if (e.message === 'Request failed with status code 406')
+        this.setState({ alert: ErrorAlert('register.406') })
+      else if (e.message === 'Request failed with status code 400')
+        this.setState({ alert: PreErrorAlert('register.400', 'text', 'info') })
+      else if (e.message === 'Request failed with status code 500')
+        this.setState({ alert: PreErrorAlert('register.500', 'text', 'info') })
+      else {
+        console.log(e.message)
+        this.setState({
+          alert: {
+            type: 'error',
+            text: 'Error occured!',
+          },
+        })
+      }
+    }
+  }
+
   render() {
+    const keyboardDetection = {
+      update: (keyboardHeight: any) => this.setState({ keyboardHeight }),
+    }
+
+    const outerBox = {
+      style: {
+        height:
+          GlobalStyles().appHeight -
+          GlobalStyles().statusbarHeight -
+          GlobalStyles().barHeight -
+          this.state.keyboardHeight,
+      },
+    }
+
     return (
-      <>
-        <Navigation label="Registration" />
-        <RegisterTitle />
-        <LoginInputEmail
-          onSubmit={() => {}}
-          onChange={(email, emailValid) => this.setState({ email, emailValid })}
-        />
-        <LoginInputPassword
-          onSubmit={() => {}}
-          onChange={(password, passwordValid) =>
-            this.setState({ password, passwordValid })
-          }
-        />
-        <LoginInputPassword
-          repeat={true}
-          onSubmit={() => {}}
-          onChange={(password, passwordValid) =>
-            this.setState({ password, passwordValid })
-          }
-        />
-        <LoginButton
-          checking={false}
-          onSubmit={() => {}}
-          disabled={false}
-          //   disabled={!(this.state.passwordValid && this.state.emailValid)}
-        />
-      </>
+      <KeyboardDetection {...keyboardDetection}>
+        <BackgroundImage>
+          <Navigation label="Registration" />
+          {this.state.alert.text !== '' && (
+            <Alert
+              yOffset={GlobalStyles().statusbarHeight}
+              onClose={() => {
+                this.setState({ alert: { text: '', type: 'error' } })
+              }}
+              {...this.state.alert}
+            />
+          )}
+          <ScrollView {...outerBox}>
+            <RegisterTitle />
+            <LoginInput
+              onChange={firstName => this.setState({ firstName })}
+              onSubmit={() => this.submit()}
+              placeholder="Vorname"
+            />
+            <LoginInput
+              onChange={lastName => this.setState({ lastName })}
+              onSubmit={() => this.submit()}
+              placeholder="Nachname"
+            />
+            <Text
+              style={{
+                marginLeft: 20,
+                color: '#fff',
+                opacity: 0.3,
+                fontSize: 12,
+                marginTop: 5,
+                marginBottom: -22,
+              }}
+            >
+              Nachname wird zu <Text style={{ fontWeight: 'bold' }}>N.</Text>{' '}
+              abgekürzt!
+            </Text>
+            <LoginInputEmail
+              onChange={(email, emailValid) =>
+                this.setState({ email, emailValid })
+              }
+              onSubmit={() => this.submit()}
+            />
+            <LoginInputPassword
+              onChange={(password, passwordValid) =>
+                this.setState({ password, passwordValid })
+              }
+              onSubmit={() => this.submit()}
+            />
+            <LoginInputPassword
+              repeat={true}
+              onChange={(passwordRepeat, passwordRepeatValid) =>
+                this.setState({ passwordRepeat, passwordRepeatValid })
+              }
+              onSubmit={() => this.submit()}
+            />
+            <LoginButton
+              checking={this.state.checking}
+              onSubmit={() => this.submit()}
+              // disabled={this.state.disabled}
+              disabled={false}
+            />
+          </ScrollView>
+        </BackgroundImage>
+      </KeyboardDetection>
     )
   }
 }
