d1c255ed297aa5092978e3c0bd8bb47bd4b4dc50
Max Kruggel <support@redigermany.de>
2021-09-19 20:04:14 +0200

    Added alert

--- src/interfaces/INotificationPageState.ts
+++ src/interfaces/INotificationPageState.ts
@@ -1,6 +1,8 @@
+import IAlertProps from './IAlertProps'
 import IAPINotification from './IAPINotification'
 
 export interface INotificationPageState {
   notifications: IAPINotification[]
   refreshing: boolean
+  alert: IAlertProps
 }
