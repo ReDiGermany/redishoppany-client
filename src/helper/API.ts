624a4939d5add3fbd2b65538397f63f2b4b4a039
Max Kruggel <support@redigermany.de>
2021-09-28 20:17:56 +0200

    Added NODE_ENV for api uri

--- src/helper/API.ts
+++ src/helper/API.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-underscore-dangle */
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import axios, { AxiosInstance } from 'axios'
 import * as Localization from 'expo-localization'
@@ -9,9 +10,10 @@ import Socket from './Socket'
 // Error Handling from https://gist.github.com/fgilio/230ccd514e9381fafa51608fcf137253
 
 export default class API {
-  public static domain = 'http://192.168.0.30:3001'
-
-  // public static domain = 'https://api.lisha-app.com'
+  public static domain =
+    process.env.NODE_ENV === 'development'
+      ? 'http://192.168.0.30:3001'
+      : 'https://api.lisha-app.com'
 
   private static config = {
     baseURL: this.domain,
@@ -62,35 +64,42 @@ export default class API {
     callback?: ICallback<T>,
     noCache: boolean = false
   ) {
-    AsyncStorage.getItem(uri).then(async strRet => {
+    return AsyncStorage.getItem(uri).then(async strRet => {
       if (strRet && !noCache) {
         const storeData: T = JSON.parse(strRet)
         callback?.(storeData)
       }
-      this.cacheFromAPI<T>(uri, callback)
       Socket.onReload(uri, () => {
         this.cacheFromAPI<T>(uri, callback)
       })
 
-      return true
+      return this.cacheFromAPI<T>(uri, callback)
     })
   }
 
   public static async cacheFromAPI<T>(uri: string, callback?: ICallback<T>) {
-    const auth = await API.getAuth()
-    const ret = await API.getInstace()
-      .axiosInstance.get(this.getUrl(uri), auth)
-      .catch(error => this.handleError(error, uri, auth, 'get'))
-    if (!ret) return null
-
-    const returnData =
-      typeof ret.data === 'object' && 'data' in ret.data
-        ? ret.data.data
-        : ret.data
-    callback?.(returnData)
-    await AsyncStorage.setItem(uri, JSON.stringify(returnData))
-
-    return true
+    return new Promise<T>((res, rej) =>
+      API.getAuth().then(auth =>
+        API.getInstace()
+          .axiosInstance.get(this.getUrl(uri), auth)
+          .then(ret => {
+            if (ret) {
+              const returnData =
+                typeof ret.data === 'object' && 'data' in ret.data
+                  ? ret.data.data
+                  : ret.data
+              if (returnData)
+                AsyncStorage.setItem(uri, JSON.stringify(returnData))
+              callback?.(returnData)
+              res(returnData)
+            }
+          })
+          .catch(error => {
+            rej(error)
+            this.handleError(error, uri, auth, 'get')
+          })
+      )
+    )
   }
 
   public static async post<T>(uri: string, data: any): Promise<T | null> {
@@ -139,19 +148,29 @@ export default class API {
     type: string,
     info: any = undefined
   ) {
-    console.log(`AXIOS::${type.toUpperCase()}::ERROR`, { uri, auth, info })
+    const title = `AXIOS::${type.toUpperCase()}::ERROR`
+    const data = { uri, auth: auth.auth, info, error }
     // Error 😨
     if (error.response) {
       // The request was made and the server responded with a status code that falls out of the range of 2xx
-      console.log('error.response', error.response.status, error.response)
+      console.log(title, 'error.response', {
+        ...data,
+        status: error.response.status,
+        response: error.response,
+      })
     } else if (error.request) {
       // The request was made but no response was received, `error.request` is an instance of XMLHttpRequest in the browser and an instance of http.ClientRequest in Node.js
-      console.log('error.request', error.request)
+      console.log(title, 'error.request', {
+        ...data,
+        body: error.request.body,
+        headers: error.request._headers,
+        url: error.request._url,
+      })
     } else {
       // Something happened in setting up the request and triggered an Error
-      console.log('Error', error.message)
+      console.log(title, 'Error', { ...data, message: error.message })
     }
 
-    return error
+    return null
   }
 }
